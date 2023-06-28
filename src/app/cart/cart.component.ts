6e48512d6c560e0cc91db91b46c4d47c6a36c3eb
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-06-28 11:23:11 -0300

    Initial commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  onSubmit(): void {
+    // Process checkout data here
+    console.warn('Your order has been submitted', {
+      ...this.checkoutForm.value,
+      ...this.items,
+    });
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+}
